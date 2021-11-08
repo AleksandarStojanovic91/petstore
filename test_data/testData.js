5ad2435a21dcc07a3a7e9b1661a5e1f487833d49
aleks <aleksandar.stojanovic.91bgd@gmail.com>
2021-11-08 10:00:25 +0100

    added negative scenarios

--- test_data/testData.js
+++ test_data/testData.js
@@ -1,6 +1,161 @@
-let data = {
-
+let GetPetsTests = {
+    "zero_id":{
+        petID:0,
+        testID:"@TC005",
+        expectedResponseCode:404,
+        expectedCode:1,
+        expectedType:"error",
+        expectedMessage:"Pet not found"
+    },
+    "empty_string_id":{
+        petID:"",
+        testID:"@TC006",
+        expectedResponseCode:405,
+        expectedCode:405,
+        expectedType:"unknown",
+        expectedMessage:"NONE"
+    },
+    "string_id":{
+        petID:"qwert",
+        testID:"@TC007",
+        expectedResponseCode:404,
+        expectedCode:404,
+        expectedType:"unknown",
+        expectedMessage:"java.lang.NumberFormatException: For input string: \"qwert\""
+    }
+}
+let DeletePetsTests = {
+    "zero_id":{
+        petID:0,
+        testID:"@TC008",
+        expectedResponseCode:404,
+        expectedType:"NONE",
+        expectedMessage:"NONE"
+    },
+    "empty_string_id":{
+        petID:"",
+        testID:"@TC009",
+        expectedResponseCode:405,
+        expectedType:"NONE",
+        expectedMessage:"NONE"
+    },
+    "string_id":{
+        petID:"qwert",
+        testID:"@TC010",
+        expectedResponseCode:404,
+        expectedType:"unknown",
+        expectedMessage:"java.lang.NumberFormatException: For input string: \"qwert\""
+    }
+}
+let PostPetsTests = {
+    "zero_id":{
+        petID:0,
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC011",
+        expectedResponseCode:404,
+        expectedCode:1,
+        expectedType:"error",
+        expectedMessage:"Pet not found"
+    },
+    "empty_string_id":{
+        petID:"",
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC012",
+        expectedResponseCode:405,
+        expectedCode:1,
+        expectedType:"error",
+        expectedMessage:"Pet not found"
+    },
+    "string_id":{
+        petID:"qwert",
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC013",
+        expectedResponseCode:500,
+        expectedCode:500,
+        expectedType:"unknown",
+        expectedMessage:"something bad happened"
+    }
+}
+let PutPetsTests = {
+    "zero_id":{
+        petID:0,
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC011",
+        expectedResponseCode:404,
+        expectedCode:1,
+        expectedType:"error",
+        expectedMessage:"Pet not found"
+    },
+    "empty_string_id":{
+        petID:"",
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC012",
+        expectedResponseCode:405,
+        expectedCode:1,
+        expectedType:"error",
+        expectedMessage:"Pet not found"
+    },
+    "string_id":{
+        petID:"qwert",
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"active",
+        testID:"@TC013",
+        expectedResponseCode:500,
+        expectedCode:500,
+        expectedType:"unknown",
+        expectedMessage:"something bad happened"
+    },
+    "invalid_status":{
+        petID:123456,
+        categoryID:1,
+        categoryName:"CategoryName",
+        name:"TestName",
+        photoUrl:"TestURL",
+        tagsID:1,
+        tagsName:"TagName",
+        status:"not good status",
+        testID:"@TC013",
+        expectedResponseCode:500,
+        expectedCode:500,
+        expectedType:"unknown",
+        expectedMessage:"something bad happened"
+    }
 }
 module.exports = {
-    data
+    GetPetsTests,DeletePetsTests,PostPetsTests,PutPetsTests
 }
--- tests/PetStoreTests.js
+++ tests/PetStoreTests.js
@@ -6,7 +6,7 @@ const testData = require('../test_data/testData');
 
 describe('Pet Store Tests - Positive',()=>{
     let ID = 0;
-    it('Should Create a Pet - Post call', async () => {
+    it('Should Create a Pet - Post call - @TC001', async () => {
         ID = api.randomNumber(1,99999);
         apiData.postPetBody.id = ID;
 
@@ -23,7 +23,8 @@ describe('Pet Store Tests - Positive',()=>{
         test(await res.body.status).to.equal(apiData.postPetBody.status);
 
     });
-    it('Should Get a Pets Details - Get call', async () => {
+    //Checking created Pet match with the Get Pet
+    it('Should Get a Pets Details - Get call - @TC002', async () => {
         api.timeout(5000)
         const res = await api.getRequest(env.url,apiData.apis.getPets.replace("$",ID),apiData.commonHeaders);
 
@@ -38,7 +39,7 @@ describe('Pet Store Tests - Positive',()=>{
         test(await res.body.status).to.equal(apiData.postPetBody.status);
 
     });
-    it('Should Edit a Pet - Put call', async () => {
+    it('Should Edit a Pet - Put call - @TC003', async () => {
         apiData.putPetBody.id = ID;
         apiData.putPetBody.name = "TestPetNameEdit";
 
@@ -55,11 +56,81 @@ describe('Pet Store Tests - Positive',()=>{
         test(await res.body.status).to.equal(apiData.putPetBody.status);
 
     });
-    it('Should Delete a Pet - Delete call', async () => {
+    it('Should Delete a Pet - Delete call - @TC004', async () => {
         const res = await api.deleteRequest(env.url,apiData.apis.getPets.replace("$",ID),apiData.commonHeaders);
 
         test(await res.statusCode).to.equal(200);
         test(await res.body.type).to.equal("unknown");
         test(await res.body.message).to.equal(ID.toString());
+    });
+});
+
+describe('Pet Store Tests - Negative',()=>{
+    //Loops through test data object and for each object runs a test updating test data and expected data
+    Object.entries(testData.GetPetsTests).forEach(([variant, data]) => {
+        it('Should not Get a Pets Details with '+variant+' '+data.testID, async () => {
+            const res = await api.getRequest(env.url,apiData.apis.getPets.replace("$",data.petID),apiData.commonHeaders);
+
+            test(await res.statusCode).to.equal(data.expectedResponseCode);
+            //Should verify different status codes based on whats expected in test data
+            test(await res.body.code).to.equal(data.expectedCode);
+            test(await res.body.type).to.equal(data.expectedType);
+            if(data.expectedMessage !== "NONE"){
+                test(await res.body.message).to.equal(data.expectedMessage);
+            }
+        });
+    });
+    Object.entries(testData.DeletePetsTests).forEach(([variant, data]) => {
+        it('Should not Delete a Pet Details with '+variant+' '+data.testID, async () => {
+
+            const res = await api.deleteRequest(env.url,apiData.apis.getPets.replace("$",data.petID),apiData.commonHeaders);
+
+            test(await res.statusCode).to.equal(data.expectedResponseCode);
+            if(data.expectedType !== "NONE") {
+                test(await res.body.type).to.equal(data.expectedType);
+            }
+            if(data.expectedMessage !== "NONE") {
+                test(await res.body.message).to.equal(data.expectedMessage);
+            }
+        });
+    });
+    Object.entries(testData.PostPetsTests).forEach(([variant, data]) => {
+        it('Should not Create a Pet Details with '+variant+' '+data.testID, async () => {
+            apiData.postPetBody.id = data.petID;
+            apiData.postPetBody.category.id = data.categoryID;
+            apiData.postPetBody.category.name = data.categoryName;
+            apiData.postPetBody.name = data.name;
+            apiData.postPetBody.photoUrls[0] = data.photoUrl;
+            apiData.postPetBody.tags.id = data.tagsID;
+            apiData.postPetBody.tags.name = data.tagsName;
+            apiData.postPetBody.status = data.status;
+
+            const res = await api.postRequest(env.url,apiData.apis.postPets,apiData.postPetBody,apiData.commonHeaders);
+
+            test(await res.statusCode).to.equal(data.expectedResponseCode);
+            test(await res.body.type).to.equal(data.expectedType);
+            test(await res.body.message).to.equal(data.expectedMessage);
+            test(await res.body.code).to.equal(data.expectedCode);
+
+        });
+    });
+    Object.entries(testData.PutPetsTests).forEach(([variant, data]) => {
+        it('Should not Update a Pet Details with '+variant+' '+data.testID, async () => {
+            apiData.putPetBody.id = data.petID;
+            apiData.putPetBody.category.id = data.categoryID;
+            apiData.putPetBody.category.name = data.categoryName;
+            apiData.putPetBody.name = data.name;
+            apiData.putPetBody.photoUrls[0] = data.photoUrl;
+            apiData.putPetBody.tags.id = data.tagsID;
+            apiData.putPetBody.tags.name = data.tagsName;
+            apiData.putPetBody.status = data.status;
+
+            const res = await api.putRequest(env.url,apiData.apis.postPets,apiData.putPetBody,apiData.commonHeaders);
+
+            test(await res.statusCode).to.equal(data.expectedResponseCode);
+            test(await res.body.type).to.equal(data.expectedType);
+            test(await res.body.message).to.equal(data.expectedMessage);
+            test(await res.body.code).to.equal(data.expectedCode);
+        });
     });
 });
